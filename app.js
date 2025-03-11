d8f3c5121f436c3eac3b96b9a1a1342395766117
Enoque12 <finokelas@gmail.com>
2025-03-11 20:06:04 +0200

    Dando uma melhorada

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let nomes = [];
-let aux = [];
 
 function adicionarAmigo() {
   let nome = document.querySelector("input").value;
